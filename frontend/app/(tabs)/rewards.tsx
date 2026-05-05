192bc02193ae593aa10ad81bb59fc09636f1fc7a
DakotaK92 <dakotaking92@gmail.com>
2026-05-05 12:55:11 -0400

    Add AppHeader pt. 2

--- frontend/app/(tabs)/rewards.tsx
+++ frontend/app/(tabs)/rewards.tsx
@@ -1,22 +1,14 @@
 import React from 'react'
-import { Text, View, Image, ImageBackground } from 'react-native'
+import { Text, View, ImageBackground } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-import SignOutButton from '../../components/SignOutButton'
+import AppHeader from '../../components/AppHeader'
 import RewardsScreen from '../../components/RewardsScreen'
 
 const Rewards = () => {
   return (
     <SafeAreaView className="flex-1 bg-transparent" edges={['top']}>
-      {/* Header Section */}
-      <View className="flex-row items-center justify-between px-4 py-3 border-b border-gray-200">
-        <View className="h-10 w-10" />
-        <Image
-          source={require("../../assets/icons/logo.png")}
-          className="h-10 w-24 items-center"
-        />
-        <SignOutButton />
-      </View>
+      <AppHeader />
 
       {/* Body Section */}
       <ImageBackground
